5d19db22c8c824a1df8cb6c6c475cab0e7c33f9e
lokeshreddy420 <35022342+lokeshreddy420@users.noreply.github.com>
2023-05-12 07:05:28 +0530

    Update2 main.tf

--- main.tf
+++ main.tf
@@ -1,94 +1,85 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_vpc" "main" {
+resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.0.0/24"
+resource "aws_subnet" "web" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 }
 
-resource "aws_subnet" "private" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "app" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-1b"
 }
 
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
+resource "aws_subnet" "db" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1c"
 }
 
-resource "aws_nat_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-  subnet_id = aws_subnet.private.id
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
 }
 
-resource "aws_load_balancer" "main" {
-  name = "my-load-balancer"
-  
-  # The subnets that the load balancer will be attached to.
+resource "aws_nat_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+  subnet_id = aws_subnet.app.id
+}
+
+resource "aws_elb" "example" {
+  name = "example-elb"
   subnets = [
-    aws_subnet.public.id,
-    aws_subnet.public.id,
+    aws_subnet.web.id,
+    aws_subnet.web.id,
   ]
-
-  # The type of load balancer.
-  type = "application"
-
-  # The health check settings for the load balancer.
-  health_check {
-    interval = 30
-    timeout = 5
-    unhealthy_threshold = 2
-    healthy_threshold = 2
-  }
-}
-
-resource "aws_rds_instance" "main" {
-  name = "my-database"
-  engine = "mysql"
-  instance_class = "db.t2.micro"
-  
-  # The VPC that the database instance will be created in.
-  vpc_id = aws_vpc.main.id
-
-  # The subnet that the database instance will be created in.
-  subnet_id = aws_subnet.private.id
-
-  # The database credentials.
-  username = "root"
-  password = "my-password"
-}
-
-resource "aws_ec2_instance" "web" {
-  name = "my-web-server"
-  ami = "ami-0123456789abcdef0"
-  instance_type = "t2.micro"
-  
-  # The VPC that the instance will be created in.
-  vpc_id = aws_vpc.main.id
-
-  # The subnet that the instance will be created in.
-  subnet_id = aws_subnet.public.id
-
-  # The security groups that the instance will be associated with.
   security_groups = [
     aws_security_group.web.id,
   ]
 }
 
+resource "aws_autoscaling_group" "example" {
+  name = "example-asg"
+  desired_capacity = 2
+  min_size = 1
+  max_size = 3
+  vpc_id = aws_vpc.example.id
+  launch_configuration = aws_launch_configuration.example.id
+  health_check_grace_period = 300
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-0123456789abcdef"
+  instance_type = "t2.micro"
+  security_groups = [
+    aws_security_group.web.id,
+  ]
+}
+
+resource "aws_rds_instance" "example" {
+  instance_class = "db.t2.micro"
+  engine = "mysql"
+  username = "root"
+  password = "password"
+  database_name = "example"
+}
+
 resource "aws_security_group" "web" {
-  name = "my-web-security-group"
-  
-  # The inbound rules for the security group.
+  name = "example-web-sg"
   ingress {
-    from_port = 80
-    to_port = 80
     protocol = "tcp"
+    port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "db" {
+  name = "example-db-sg"
+  ingress {
+    protocol = "tcp"
+    port = 3306
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
