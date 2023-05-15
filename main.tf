164580a84ab951d229d0d89cb31e8e0556577884
lokeshreddy420 <35022342+lokeshreddy420@users.noreply.github.com>
2023-05-15 18:36:18 +0530

    Update3 main.tf

--- main.tf
+++ main.tf
@@ -1,85 +1,119 @@
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+# Configure the AWS provider
+provider "aws" {
+  region = "us-east-1"
 }
 
-resource "aws_subnet" "web" {
-  vpc_id = aws_vpc.example.id
+# Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+# Create a public subnet for the presentation layer
+resource "aws_subnet" "presentation_public" {
+  vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Presentation Public"
+  }
 }
 
-resource "aws_subnet" "app" {
-  vpc_id = aws_vpc.example.id
+# Create a private subnet for the presentation layer
+resource "aws_subnet" "presentation_private" {
+  vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Presentation Private"
+  }
 }
 
-resource "aws_subnet" "db" {
-  vpc_id = aws_vpc.example.id
+# Create a public subnet for the application layer
+resource "aws_subnet" "application_public" {
+  vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Application Public"
+  }
+}
+
+# Create a private subnet for the application layer
+resource "aws_subnet" "application_private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Application Private"
+  }
+}
+
+# Create a private subnet for the database layer
+resource "aws_subnet" "database" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.5.0/24"
   availability_zone = "us-east-1c"
-}
 
-resource "aws_internet_gateway" "example" {
-  vpc_id = aws_vpc.example.id
-}
-
-resource "aws_nat_gateway" "example" {
-  vpc_id = aws_vpc.example.id
-  subnet_id = aws_subnet.app.id
-}
-
-resource "aws_elb" "example" {
-  name = "example-elb"
-  subnets = [
-    aws_subnet.web.id,
-    aws_subnet.web.id,
-  ]
-  security_groups = [
-    aws_security_group.web.id,
-  ]
-}
-
-resource "aws_autoscaling_group" "example" {
-  name = "example-asg"
-  desired_capacity = 2
-  min_size = 1
-  max_size = 3
-  vpc_id = aws_vpc.example.id
-  launch_configuration = aws_launch_configuration.example.id
-  health_check_grace_period = 300
-}
-
-resource "aws_launch_configuration" "example" {
-  image_id = "ami-0123456789abcdef"
-  instance_type = "t2.micro"
-  security_groups = [
-    aws_security_group.web.id,
-  ]
-}
-
-resource "aws_rds_instance" "example" {
-  instance_class = "db.t2.micro"
-  engine = "mysql"
-  username = "root"
-  password = "password"
-  database_name = "example"
-}
-
-resource "aws_security_group" "web" {
-  name = "example-web-sg"
-  ingress {
-    protocol = "tcp"
-    port = 80
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "Database"
   }
 }
 
-resource "aws_security_group" "db" {
-  name = "example-db-sg"
-  ingress {
-    protocol = "tcp"
-    port = 3306
-    cidr_blocks = ["0.0.0.0/0"]
+# Create an Internet Gateway and attach it to the VPC
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Main"
   }
 }
+
+# Create a Route Table for the VPC
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+# Associate the public subnets with the Route Table
+resource "aws_route_table_association" "presentation_public" {
+  subnet_id      = aws_subnet.presentation_public.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_route_table_association" "application_public" {
+  subnet_id      = aws_subnet.application_public.id
+  route_table_id = aws_route_table.main.id
+}
+
+# Create a Security Group for the ALB
+resource "aws_security_group" "alb" {
+  name_prefix = "alb-"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0
